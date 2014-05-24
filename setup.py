aec73a264237af76fc7f808955c9e697daab9ef3
Godefroid Chapelle <gotcha@bubblenet.be>
2014-05-24 19:29:35 +0200

    first import

---
+++ setup.py
@@ -0,0 +1,54 @@
+from setuptools import setup, find_packages
+
+version = '1.0'
+
+long_description = (
+    open('README.txt').read()
+    + '\n' +
+    open('CONTRIBUTORS.txt').read()
+    + '\n' +
+    open('CHANGES.txt').read()
+    + '\n')
+
+setup(
+    name='cpskin.minisite',
+    version=version,
+    description="UI for sections configured with their own domain",
+    long_description=long_description,
+    # Get more strings from
+    # http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        "Environment :: Web Environment",
+        "Operating System :: OS Independent",
+        "Framework :: Zope2",
+        "Framework :: Plone",
+        "Framework :: Plone :: 4.2",
+        "Framework :: Plone :: 4.3",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+    keywords='',
+    author='',
+    author_email='',
+    url='http://svn.plone.org/svn/collective/',
+    license='gpl',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    namespace_packages=['cpskin', ],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'setuptools',
+        'Plone',
+    ],
+    extras_require={
+        'test': [
+            'plone.app.testing',
+            'plone.app.robotframework',
+        ],
+    },
+    entry_points="""
+    """
+)
