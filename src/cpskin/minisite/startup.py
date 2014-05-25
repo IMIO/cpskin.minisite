d553a3cf9b9279c79adf3557e97ec1bb2ae94673
Godefroid Chapelle <gotcha@bubblenet.be>
2014-05-25 16:09:34 +0200

    configure from file system

--- src/cpskin/minisite/startup.py
+++ src/cpskin/minisite/startup.py
@@ -1,18 +1,45 @@
+import ConfigParser
+import os.path
+
 from zope.component import provideUtility
 
 from cpskin.minisite.minisite import Minisite
 
 
+
 def registerMinisites(event):
-    minisite = Minisite(
-        'http://main/domain/url',
-        'http://minisite/url',
-        'plone',
-    )
-    provideUtility(
-        minisite,
-        name=u"/plone/minisite",
-    )
+    minisites_directory = os.path.join(CLIENT_HOME, 'minisites')
+    if os.path.exists(minisites_directory):
+        registerMinisitesFromDirectory(minisites_directory)
+
+def registerMinisitesFromDirectory(directory):
+    files = os.listdir(directory)
+    for filename in files:
+        filename = os.path.join(directory, filename)
+        if os.path.isfile(filename):
+             registerMinisitesFromFile(filename)
+
+def registerMinisitesFromFile(filename):
+    config = ConfigParser.RawConfigParser()
+    try:
+        config.read(filename)
+    except ConfigParser.MissingSectionHeaderError:
+        return
+    for section in config.sections():
+        try:
+            search_path = config.get(section, 'search_path')
+            minisite_url = config.get(section, 'minisite_url')
+            minisite = Minisite(
+                main_portal_url=section,
+                minisite_url=minisite_url,
+                search_path=search_path,
+            )
+            provideUtility(
+                minisite,
+                name=search_path,
+            )
+        except KeyError:
+            continue
 
 
 def registerMinisitesSetupHandler(context):
@@ -24,4 +51,11 @@ def registerMinisitesSetupHandler(context):
     if context.readDataFile(
             'register_cpskin_minisites.txt') is None:
         return
-    registerMinisites(None)
+    from cpskin.minisite import tests
+    
+    filename = os.path.join(
+        os.path.dirname(tests.__file__), 
+        'minisites_config.txt',
+    )
+    
+    registerMinisitesFromFile(filename)
