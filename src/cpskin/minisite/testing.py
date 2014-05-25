d8de4371f5deea24ea7fa64cc3161b95c3135d37
Godefroid Chapelle <gotcha@bubblenet.be>
2014-05-25 11:03:18 +0200

    first import was not complete

---
+++ src/cpskin/minisite/testing.py
@@ -0,0 +1,23 @@
+from plone.app.testing import PloneWithPackageLayer
+from plone.app.testing import IntegrationTesting
+from plone.app.testing import FunctionalTesting
+
+from plone.testing import z2
+
+import cpskin.minisite
+
+CPSKIN_MINISITE = PloneWithPackageLayer(
+    name='CPSKIN_MINISITE',
+    zcml_package=cpskin.minisite,
+    zcml_filename='configure.zcml',
+    gs_profile_id='cpskin.minisite:default',
+)
+
+CPSKIN_MINISITE_INTEGRATION_TESTING = IntegrationTesting(
+    bases=(CPSKIN_MINISITE,),
+    name="CPSKIN_MINISITE_INTEGRATION_TESTING"
+)
+CPSKIN_MINISITE_FUNCTIONAL_TESTING = FunctionalTesting(
+    bases=(CPSKIN_MINISITE, z2.ZSERVER_FIXTURE),
+    name="CPSKIN_MINISITE_FUNCTIONAL_TESTING"
+)
