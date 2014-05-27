5f4e087c85db91955851c6c7c2b8313dcedb5779
Godefroid Chapelle <gotcha@bubblenet.be>
2014-05-28 00:36:26 +0200

    customize searchbox viewlet

--- src/cpskin/minisite/viewlet.py
+++ src/cpskin/minisite/viewlet.py
@@ -1,6 +1,13 @@
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+
 from plone.app.layout.viewlets.common import ViewletBase
+from plone.app.layout.viewlets.common import SearchBoxViewlet as SearchBoxBase
 
 
 class MinisiteViewlet(ViewletBase):
 
     pass
+
+
+class SearchBoxViewlet(SearchBoxBase):
+    index = ViewPageTemplateFile('searchbox.pt')
