3b722f12e282e67c308f9eb42fee49393bab335f
Godefroid Chapelle <gotcha@bubblenet.be>
2014-05-28 01:48:48 +0200

    Fix when minisite is a subfolder

--- src/cpskin/minisite/traverser.py
+++ src/cpskin/minisite/traverser.py
@@ -2,6 +2,8 @@ from zope.component import queryUtility
 
 from ZPublisher.BaseRequest import DefaultPublishTraverse
 
+from plone.app.imaging.traverse import ImageTraverser
+
 from cpskin.minisite.interfaces import IMinisiteConfig
 from cpskin.minisite.minisite import decorateRequest
 
@@ -9,8 +11,18 @@ from cpskin.minisite.minisite import decorateRequest
 class MinisiteTraverser(DefaultPublishTraverse):
 
     def publishTraverse(self, request, name):
-        path = "/".join(self.context.getPhysicalPath() + (name,))
+        path = "/".join(self.context.getPhysicalPath())
         config = queryUtility(IMinisiteConfig, name=path)
         decorateRequest(request, config)
         return super(MinisiteTraverser, self).publishTraverse(
+            request, name)
+
+
+class MinisiteImageTraverser(ImageTraverser):
+
+    def publishTraverse(self, request, name):
+        path = "/".join(self.context.getPhysicalPath())
+        config = queryUtility(IMinisiteConfig, name=path)
+        decorateRequest(request, config)
+        return super(MinisiteImageTraverser, self).publishTraverse(
             request, name)
