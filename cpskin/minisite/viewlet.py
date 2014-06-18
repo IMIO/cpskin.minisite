d0e7e74c438b38d2cfb3fbb08e5e33b5cb442215
Godefroid Chapelle <gotcha@bubblenet.be>
2014-06-18 15:59:26 +0200

    link should point to same page in portal mode
    in minisite viewlet

--- cpskin/minisite/viewlet.py
+++ cpskin/minisite/viewlet.py
@@ -1,11 +1,25 @@
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 
+from Products.CMFCore.utils import getToolByName
+
 from plone.app.layout.viewlets.common import ViewletBase
 from plone.app.layout.viewlets.common import SearchBoxViewlet as SearchBoxBase
 
 
 class MinisiteViewlet(ViewletBase):
-    pass
+
+    def url_in_portal_mode(self):
+        portal_url = getToolByName(self.context, 'portal_url')
+        relative_url = portal_url.getRelativeContentURL(self.context)
+        portal = portal_url.getPortalObject()
+        main_portal_url = self.request.cpskin_minisite.main_portal_url
+        minisite_url = self.request.cpskin_minisite.minisite_url
+        root_url = portal.absolute_url()
+        root_url_in_portal_mode = root_url.replace(
+            minisite_url,
+            main_portal_url
+        )
+        return '/'.join((root_url_in_portal_mode, relative_url))
 
 
 class SearchBoxViewlet(SearchBoxBase):
