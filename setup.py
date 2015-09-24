ab8483f5a510a20d63ee4d77ed1a1f01d1a91c13
boulch <christophe.boulanger@imio.be>
2015-09-24 10:36:04 +0200

    add cpskin.locales in install_requires

--- setup.py
+++ setup.py
@@ -41,6 +41,7 @@ setup(
     zip_safe=False,
     install_requires=[
         'setuptools',
+        'cpskin.locales',
         'Plone',
         'plone.api',
         'collective.weightedportlets',
