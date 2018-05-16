bf3d66ea0ba557a3e80012d07e19df1b8797eb1d
Benoît Suttor <bsuttor@imio.be>
2018-05-16 10:10:54 +0200

    Override REST publishTraverse

--- cpskin/minisite/traverser.py
+++ cpskin/minisite/traverser.py
@@ -4,16 +4,24 @@ from cpskin.minisite.interfaces import IMinisiteConfig
 from cpskin.minisite.minisite import decorateRequest
 from cpskin.minisite.portlet import checkPortlet
 from plone.app.imaging.traverse import ImageTraverser
+from plone.rest.interfaces import IAPIRequest
 from plone.rest.traverse import RESTTraverse
 from Products.CMFCore.interfaces import IContentish
 from zope.component import queryUtility
+from ZPublisher.BaseRequest import DefaultPublishTraverse
 
 
 class MinisiteTraverser(RESTTraverse):
 
     def publishTraverse(self, request, name):
-        result = super(MinisiteTraverser, self).publishTraverse(
-            request, name)
+        if IAPIRequest.providedBy(request):
+            result = RESTTraverse.publishTraverse(self, request, name)
+        else:
+            result = DefaultPublishTraverse.publishTraverse(
+                self,
+                request,
+                name
+            )
         if IContentish.providedBy(result):
             path = '/'.join(result.getPhysicalPath())
             logger.debug('Traversing {0}'.format(path))
