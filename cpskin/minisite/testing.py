87d8866b2096f650b9d8eb74bb2d2db4272f6ea3
Benoit Suttor <bsuttor@imio.be>
2018-06-01 15:31:12 +0200

    Fix some robot tests

--- cpskin/minisite/testing.py
+++ cpskin/minisite/testing.py
@@ -1,19 +1,35 @@
 # -*- coding: utf-8 -*-
 # from plone import api
+from cpskin.core.behaviors.metadata import IHiddenTags
+from cpskin.core.utils import add_behavior
 from plone.app.controlpanel.security import ISecuritySchema
 from plone.app.robotframework.testing import REMOTE_LIBRARY_BUNDLE_FIXTURE
+from plone.app.testing import applyProfile
 from plone.app.testing import FunctionalTesting
 from plone.app.testing import IntegrationTesting
 from plone.app.testing import PloneWithPackageLayer
 from plone.testing import z2
-
 import cpskin.minisite
 
 
 class CPSkinMinisiteLayer(PloneWithPackageLayer):
 
+    def setUpZope(self, app, configurationContext):
+        super(CPSkinMinisiteLayer, self).setUpZope(
+            app,
+            configurationContext)
+        z2.installProduct(app, 'Products.DateRecurringIndex')
+
+    def tearDownZope(self, app):
+        # Uninstall products installed above
+        z2.uninstallProduct(app, 'Products.DateRecurringIndex')
+
     def setUpPloneSite(self, portal):
-        super(CPSkinMinisiteLayer, self).setUpPloneSite(portal)
+        # super(CPSkinMinisiteLayer).setUpPloneSite(portal)
+        portal.portal_workflow.setDefaultChain('simple_publication_workflow')
+        # applyProfile(portal, 'plone.app.contenttypes:plone-content')
+        applyProfile(portal, 'cpskin.minisite:testing')
+        add_behavior('Document', IHiddenTags.__identifier__)
         ISecuritySchema(portal).set_enable_self_reg(True)
 
 
