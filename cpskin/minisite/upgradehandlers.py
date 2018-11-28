fd00e06f7a82465efa7501a4df28a7d2de0be2fd
Benoit Suttor <bsuttor@imio.be>
2018-11-28 13:33:47 +0100

    Lint

--- cpskin/minisite/upgradehandlers.py
+++ cpskin/minisite/upgradehandlers.py
@@ -1,15 +1,19 @@
 # -*- coding: utf-8 -*-
+PROFILE_ID = 'profile-cpskin.minisite:default'
 
 
 def add_actions_viewlet(context):
-    context.runImportStepFromProfile('profile-cpskin.minisite:default', 'viewlets')
+    context.runImportStepFromProfile(PROFILE_ID, 'viewlets')
 
 
 def move_cpskin_actions(context):
-    context.runImportStepFromProfile('profile-cpskin.minisite:to0003', 'actions')
-    context.runImportStepFromProfile('profile-cpskin.minisite:default', 'actions')
+    context.runImportStepFromProfile(
+        'profile-cpskin.minisite:to0003',
+        'actions'
+    )
+    context.runImportStepFromProfile(PROFILE_ID, 'actions')
 
 
 def add_minisite_menu(context):
-    context.runImportStepFromProfile('profile-cpskin.minisite:default', 'viewlets')
-    context.runImportStepFromProfile('profile-cpskin.minisite:default', 'actions')
+    context.runImportStepFromProfile(PROFILE_ID, 'viewlets')
+    context.runImportStepFromProfile(PROFILE_ID, 'actions')
